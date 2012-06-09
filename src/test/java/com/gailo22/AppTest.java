6c24a0468c72b99ecd200018172a74ef1295f073
gailo22 <gailo22@gmail.com>
2012-06-09 21:23:37 +0800

    create maven structure

---
+++ src/test/java/com/gailo22/AppTest.java
@@ -0,0 +1,14 @@
+package com.gailo22;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest {
+	
+	@Test
+	public void shouldPass() {
+		assertTrue(true);
+	}
+}
